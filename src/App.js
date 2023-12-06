329e92993d2cf7789c47f9938db4456e62a3188b
Samicostox <82226420+Samicostox@users.noreply.github.com>
2023-12-06 16:48:12 +0000

    finally

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import UserFlowChart from './userflowchart';
 import DataModelDiagram from './datamodeldiagram';
+import ChatBot from './Qodly';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <DataModelDiagram />
+        
+        <ChatBot></ChatBot>
       </header>
     </div>
   );
